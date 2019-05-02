086b871075306348e623b1c91a1f3554e7c2706d
Matheus Apolonio <matheus.apolonio@hotmail.com>
2019-05-02 16:39:59 -0300

    Fixed status bar color based on active screen

--- App.js
+++ App.js
@@ -3,11 +3,15 @@ import React, { Component } from "react";
 import { Provider } from "react-redux";
 import store from "./src/redux/store";
 
+import { StatusBar } from 'react-native';
+
 import { createReduxBoundAddListener } from 'react-navigation-redux-helpers';
 import AppNavigationState from './src/navigators/AppNavigator';
 
 export default class App extends Component {
   render() {
+    StatusBar.setBarStyle('light-content');
+
     return (
       <Provider store={store}>
         <AppNavigationState listener={createReduxBoundAddListener('root')} />
